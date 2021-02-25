2b8281509e3d075582bb89670ff1edffe7175d0a
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-02-25 15:58:00 -0500

    Updated Footer.js link

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="Footer mt-3">
       <a
         className="btn btn-secondary"
-        href="https://github.com/KayRich-cmd/weather-app-project"
+        href="https://github.com/KayRich-cmd/react-weather-app"
         target="_blank"
         rel="noreferrer"
         role="button"
