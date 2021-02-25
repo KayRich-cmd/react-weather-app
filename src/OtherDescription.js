7bbba02c916c108aeaf16772a2c1bc22c06a76ac
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-02-25 15:45:55 -0500

    Started Updating Other Description component

--- src/OtherDescription.js
+++ src/OtherDescription.js
@@ -9,18 +9,16 @@ export default function OtherDescription() {
   };
   return (
     <div className="OtherDescription">
-      <div className="col-4">
-        <ul>
-          <li>
-            <span className="other-description-label"> Humidity:</span>
-            <span>{weatherData.humidity}%</span>
-          </li>
-          <li>
-            <span className="other-description-label"> Wind Speed:</span>
-            <span>{weatherData.wind}</span> km/h
-          </li>
-        </ul>
-      </div>
+      <ul>
+        <li>
+          <span className="other-description-label"> Humidity:</span>
+          <span>{weatherData.humidity}%</span>
+        </li>
+        <li>
+          <span className="other-description-label"> Wind Speed:</span>
+          <span>{weatherData.wind}</span> km/h
+        </li>
+      </ul>
     </div>
   );
 }
