19e5fe881f843d58e8d8039fc466453574596da1
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-02-27 20:24:33 -0500

    Updated OtherDescription.js & Weather.css

--- src/OtherDescription.js
+++ src/OtherDescription.js
@@ -11,11 +11,11 @@ export default function OtherDescription() {
     <div className="OtherDescription">
       <ul>
         <li>
-          <span className="other-description-label"> Humidity:</span>
+          <span className="other-description-label"> Humidity:</span>{" "}
           <span>{weatherData.humidity}%</span>
         </li>
         <li>
-          <span className="other-description-label"> Wind Speed:</span>
+          <span className="other-description-label"> Wind Speed:</span>{" "}
           <span>{weatherData.wind}</span> km/h
         </li>
       </ul>
