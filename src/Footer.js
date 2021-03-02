611b2c1ac7c14406aacd5b574767994841529424
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-03-01 22:51:58 -0500

    Updated Footer.css

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="Footer mt-3">
+    <footer className="Footer mb-5">
       <a
         className="btn btn-secondary"
         href="https://github.com/KayRich-cmd/react-weather-app"
