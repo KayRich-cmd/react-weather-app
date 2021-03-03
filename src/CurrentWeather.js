b6e35f52e8e6c4e37c1322facf9358b8ee3c14d9
Kay Richardson <73449592+KayRich-cmd@users.noreply.github.com>
2021-03-03 17:35:52 -0500

    Updated manifest.json
    Removed logo512.png

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -6,40 +6,20 @@ export default function CurrentWeather() {
   const [ready, setReady] = useState(false);
   const [weatherData, setWeatherData] = useState({});
 
-  function handleResponse(response) {
-    console.log(response.data);
-    setWeatherData({
-      temperature: response.data.main.temp,
-      wind: 2,
-      city: response.data.name,
-    });
-    setReady(true);
-  }
-
-  if (ready) {
-    return (
-      <div className="CurrentWeather">
-        <div className="row">
-          <div className="col-12">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/48/sunny.png"
-              alt="Sunny"
-            />
-          </div>
+  return (
+    <div className="CurrentWeather">
+      <div className="row">
+        <div className="col-12">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/48/sunny.png"
+            alt="Sunny"
+          />
         </div>
-        <span>{Math.round(weatherData.temperature)}°</span>
-        <span className="unit-type">
-          <a href="/">C</a> |<a href="/"> F</a>
-        </span>
       </div>
-    );
-  } else {
-    const apiKey = "2e85ceac5b7aab61ec3567d2389a4fd2";
-    let city = "New York";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-
-    return "Loading...";
-  }
+      <span>3°</span>
+      <span className="unit-type">
+        <a href="/">C</a> |<a href="/"> F</a>
+      </span>
+    </div>
+  );
 }
